63cc5c129983217396ac2b2ada804042ab0363bf
Hardeep <hardeep.singh@lpinfotech.com>
2023-03-28 10:22:24 +0530

    update sass theming

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let main = document.getElementById("myButton");
 let mainButton = main.getElementsByClassName("btn");
-for (const element of mainButton) {
-  element.addEventListener("click", function () {
+for (let btnBox of mainButton) {
+  btnBox.addEventListener("click", function () {
     let current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
