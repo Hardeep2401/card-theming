9253f1655d8fa4c85f8e82e0c3c47f20d62623d4
Hardeep <hardeep.singh@lpinfotech.com>
2023-03-29 18:28:13 +0530

    update theme

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 let main = document.getElementById("myButton");
-let mainButton = main.getElementsByClassName("btn");
+let mainButton = main.querySelectorAll(".btn");
 for (let btnBox of mainButton) {
   btnBox.addEventListener("click", function () {
     let current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
+    let theme = document.getElementsByClassName("btn active").innerHTML = "";
+    document.getElementById("#myButton").classList.remove();
+    document.getElementById("#myButton").classList.add("theme");
   });
 }
